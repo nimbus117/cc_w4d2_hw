98073b4ec22d7235a934ee0468e085847a93d38b
nimbus117 <mail@nimbus117.co.uk>
2018-08-21 16:46:21 +0100

    added result view

--- controller.rb
+++ controller.rb
@@ -3,5 +3,6 @@ require 'sinatra/contrib/all'
 require_relative 'models/game.rb'
 
 get '/:p1/:p2' do
-  Game.play(params[:p1], params[:p2])
+  @game_result = Game.play(params[:p1], params[:p2])
+  erb :result
 end
