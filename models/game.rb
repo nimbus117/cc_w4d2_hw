ea5dc38487d99bfb2be1a9083e6daeb2a8661498
nimbus117 <mail@nimbus117.co.uk>
2018-08-21 17:26:02 +0100

    added player 1/2 wins message

--- models/game.rb
+++ models/game.rb
@@ -1,18 +1,21 @@
 class Game
 
   def Game.play(p1, p2)
-    valid_answers = ['rock', 'paper', 'scissors']
+
     p1.downcase!
     p2.downcase!
-  case
-  when !(valid_answers.include? p1) || !(valid_answers.include? p2)
-    "invalid"
-  when p1 == p2
-    "draw"
-  when (p1 == 'rock' && p2 == 'scissors') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'scissors' && p2 == 'paper')
-    "#{p1} wins"
-  else
-    "#{p2} wins"
-  end
+
+    valid_answers = ['rock', 'paper', 'scissors']
+
+    case
+    when !(valid_answers.include? p1) || !(valid_answers.include? p2)
+      "invalid"
+    when p1 == p2
+      "draw"
+    when (p1 == 'rock' && p2 == 'scissors') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'scissors' && p2 == 'paper')
+      "Player 1 wins by playing #{p1}"
+    else
+      "Player 2 wins by playing #{p2}"
+    end
   end
 end
