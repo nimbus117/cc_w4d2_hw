bf68a3176932b1854e8cf610bb2046e9cb5365f2
nimbus117 <mail@nimbus117.co.uk>
2018-08-21 16:39:20 +0100

    refactored to use case statement

--- models/game.rb
+++ models/game.rb
@@ -4,15 +4,15 @@ class Game
     valid_answers = ['rock', 'paper', 'scissors']
     p1.downcase!
     p2.downcase!
-
-    if !(valid_answers.include? p1) || !(valid_answers.include? p2)
-      "invalid"
-    elsif p1 == p2
-      "draw"
-    elsif (p1 == 'rock' && p2 == 'scissors') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'scissors' && p2 == 'paper')
-      "#{p1} wins"
-    else
-      "#{p2} wins"
-    end
+  case
+  when !(valid_answers.include? p1) || !(valid_answers.include? p2)
+    "invalid"
+  when p1 == p2
+    "draw"
+  when (p1 == 'rock' && p2 == 'scissors') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'scissors' && p2 == 'paper')
+    "#{p1} wins"
+  else
+    "#{p2} wins"
+  end
   end
 end
