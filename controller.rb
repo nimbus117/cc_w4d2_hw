08e6172817371c365b2d2da0d324e016b7e978a0
nimbus117 <mail@nimbus117.co.uk>
2018-08-21 16:56:24 +0100

    added home page

--- controller.rb
+++ controller.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 require_relative 'models/game.rb'
 
+
 get '/:p1/:p2' do
   @game_result = Game.play(params[:p1], params[:p2])
   erb :result
+end
+
+get '/' do
+  erb :home
 end
