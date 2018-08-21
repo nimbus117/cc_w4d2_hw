e3acc28fda0102a65ab35284788ddeff05e3fcd2
nimbus117 <mail@nimbus117.co.uk>
2018-08-21 17:21:39 +0100

    added css

--- controller.rb
+++ controller.rb
@@ -2,12 +2,11 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 require_relative 'models/game.rb'
 
+get '/' do
+  erb :home
+end
 
 get '/:p1/:p2' do
   @game_result = Game.play(params[:p1], params[:p2])
   erb :result
-end
-
-get '/' do
-  erb :home
 end
